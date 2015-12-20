4d2fdb1da11a6d7b705000b85b5df43ace80df01
hyenoo <hyerin16@gmail.com>
2015-12-20 22:42:44 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
     
 var init = function() {
     // TODO:: Do your initialization job
-    console.log("init() called");   
+    console.log("init() called");
     var cnt = 0;
-    var scnt = 0;    
+    var scnt = 0;
     var snapshots = [];
-    var sbdArr = [];    
+    var sbdArr = [];
     var preArr = new Array(256);
     var curArr = new Array(256);
     for (var i = 0; i < 256; i++) {
@@ -14,17 +14,21 @@ var init = function() {
         curArr[i] = 0;
     }
     var mode = 0;
-    var endts = 0;    
-    var v = document.getElementById('v');    
-    var canvas = document.getElementById('c');
-    var context = canvas.getContext('2d');
-    var back = document.createElement('canvas');
-    var backcontext = back.getContext('2d');    
-    var tempts=v.duration;
-    var stop_play = 0;    
+    var endts = 0;
+    var preT;
+    var v = document.getElementById('v');
     v.width = 300;
     v.height = 200;
     v.loop = false;
+    var canvas = document.getElementById('c');
+    var context = canvas.getContext('2d');
+    var back = document.createElement('canvas');
+    var backcontext = back.getContext('2d');
+    var cw, ch;
+    var node;
+    var myc;
+    var tempts=v.duration;
+    var stop_play = 0;
    
     document.getElementById("buttonbar").style.display = "block";
     function setTime(tValue) {       
@@ -32,29 +36,27 @@ var init = function() {
                     v.currentTime = tValue;
                 else 
                     v.currentTime += tValue;                
-     }      
-   
+     }
     document.getElementById("stop").addEventListener("click", function(evt){
-    	button = evt.target;
+    	button = evt.target; //  get the button id to swap the text based on the state      
         if (stop_play % 2 == 0) {
         	v.pause();
             tempts = v.currentTime;
-            button.textContent = "Restart";
+            button.textContent = "RESTART";
             stop_play = 1;
         } else {
-        	 mode = 1;
+        	 mode = 2;
              v.load();
              v.currentTime = tempts;
              v.play();
-             button.textContent = "  STOP  ";
+             button.textContent = "  STOP   ";
              stop_play = 0;
         }
     }, false);
-    
     (function localFileVideoPlayerInit(win) {
         var URL = win.URL || win.webkitURL,
             displayMessage = (function displayMessageInit() {
-                var node = document.querySelector('#message');
+                node = document.querySelector('#message');
                 return function displayMessage(message, isError) {
                     node.innerHTML = message;
                     node.className = isError ? 'error' : 'info';
@@ -70,33 +72,25 @@ var init = function() {
                 if (type == "video/mp4") 
                 	v.play();        
                 else {
-                	alert("ERROR : It's NOT MP4 FILE");
+                	alert("ERROR : MP4");
                 	return false;
                 }             
             },
             inputNode = document.querySelector('input');
         if (!URL) {
-            displayMessage('Your browser is not ' +
-                '<a href="http://caniuse.com/bloburls">supported</a>!', true);
             return;
         }
         inputNode.addEventListener('change', playSelectedFile, false);
     }(window));
-    
-    v.addEventListener('ended',function(){
-    	v.controls = true;
-    	var tempctx = snapshots[scnt-1].getContext('2d');
-    	tempctx.fillText(v.duration.toFixed(2),60, 70);
+    v.addEventListener('ended',function(){v.controls = true;
+    var tempctx = snapshots[scnt-1].getContext('2d');
+	   tempctx.fillText(v.duration.toFixed(2),60, 70);
 	   }, false);
-    
-    v.addEventListener('pause',function(){
-    	mode=0;
-    	}, false);
-    
+    v.addEventListener('pause',function(){mode=0;}, false);
     v.addEventListener('play', function() {
         console.log("play ");
-        var cw = 128; 
-        var ch = 64; 
+        cw = 128; //v.width;
+        ch = 64; //v.height;
         canvas.width = cw;
         canvas.height = ch;
         back.width = cw;
@@ -107,7 +101,7 @@ var init = function() {
             if (mode == 1) 
             	sectionPlay(v);
             else
-                shotchangedetector(v, context, backcontext, cw, ch);            	
+                draw(v, context, backcontext, cw, ch);            	
         }, false);
     }, false);
 
@@ -121,50 +115,50 @@ var init = function() {
         setTimeout(sectionPlay, 20, v);
     }
 
-    function shotchangedetector(v, c, bc, w, h) {
+    function draw(v, c, bc, w, h) {
         if (v.paused || v.ended) return false;
-        
         for (var i = 0; i < 256; i++) {
             preArr[i] = curArr[i];
             curArr[i] = 0;
         }
-        
         bc.drawImage(v, 0, 0, w, h);
         var idata = bc.getImageData(0, 0, w, h);
         var data = idata.data;
-        
         var sp = 0;
-        
        	for (var j=0; j<data.length; j+=4) {
-    	  	var r = data[j];
-              var g = data[j + 1];
-              var b = data[j+ 2];
-              var brightness = (3 * r + 4 * g + b) >>> 3;
-              curArr[brightness] = curArr[brightness]+1;
-    	}
-       	
+	  	var r = data[j];
+          var g = data[j + 1];
+          var b = data[j+ 2];
+          var brightness = (3 * r + 4 * g + b) >>> 3;
+          curArr[brightness] = curArr[brightness]+1;
+	}
         var a;
-        
         if (cnt > 0) {
             var dist = 0;
             for (i = 0; i < 256; i++) {
                 dist += Math.abs(curArr[i] - preArr[i]);
             }
-            if (dist >  5000) {
-            	   var tempctx = snapshots[scnt-1].getContext('2d');
+            if (dist > 5000) {
+            	if (curArr[0]>125) {
+            		for (i=0; i<256; i++) {
+            			curArr[i] = preArr[i];
+            		}
+            	} else {
+            		var tempctx = snapshots[scnt-1].getContext('2d');
               	   tempctx.fillText(v.currentTime.toFixed(2),65, 70);
-                   myc = capture(v, scnt);
+                   myc = capture(v, 0.3, scnt);
                    snapshots.push(myc);
                    snapshots[scnt].addEventListener("click", clickEvent(scnt, v));
                        for (i = 0; i < scnt+1 ; i++) {
                            output.appendChild(snapshots[i]);
                        }
                        sbdArr.push(v.currentTime);
-                       scnt++;       	      	                   	  
-               
+                       scnt++; 
+            	}       	                   	  
+                   
             }
         } else {
-            myc = capture(v, scnt);
+            myc = capture(v, 0.3, scnt);
             snapshots.push(myc);
             snapshots[scnt].addEventListener("click", clickEvent(scnt, v));
             for (i = 0; i < scnt + 1; i++) {
@@ -172,15 +166,17 @@ var init = function() {
             }
             sbdArr.push(v.currentTime);
             scnt++;
-        }        
+        }
         cnt = cnt + 1;
-        
-        setTimeout(shotchangedetector, 20, v, c, bc, w, h);
+        setTimeout(draw, 20, v, c, bc, w, h);
     }
 
-    function capture(video, num) {        
-        var w = 95+10;
-        var h = 60+20;
+    function capture(video, scaleFactor, num) {
+        if (scaleFactor == null) {
+            scaleFactor = 1;
+        }
+        var w = 95+10;// * scaleFactor;
+        var h = 60+20;// * scaleFactor;
         var mycanvas = document.createElement('canvas');
         mycanvas.width = w;
         mycanvas.height = h;
@@ -202,11 +198,13 @@ var init = function() {
                 v.play();
                 v.currentTime = sbdArr[i];
                 endts = sbdArr[i + 1];
+                //sectionPlay(v);
             } else {
                 mode = 1;
                 v.play();
                 v.currentTime = sbdArr[i];
                 endts = v.duration;
+                //sectionPlay(v);
             }
 
         };
